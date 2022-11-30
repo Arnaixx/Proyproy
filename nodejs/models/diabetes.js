6565159c1e8d2be71c3d8421cb03c3aa71501950
Jessie Linas <112135061+jesslinross12@users.noreply.github.com>
2022-11-29 19:05:22 -0600

    mmm

--- nodejs/models/diabetes.js
+++ nodejs/models/diabetes.js
@@ -3,10 +3,6 @@ const Sequelize = require('sequelize');
 const Diabetes = (sequelize)=>{
     sequelize.define('Diabetes',{
         //Forma especifica de declarar atributos
-        nombrePersona:{
-            type: Sequelize.STRING,
-            allowNull: true
-        },
         age: Sequelize.INTEGER,
         bloodpressure: Sequelize.INTEGER,
         bmi: Sequelize.INTEGER,
