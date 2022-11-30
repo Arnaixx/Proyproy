0c04bf54c2a04be3fe217dd3ced46b0e1b8446e4
Marco Moreno <91648390+ShikurM56@users.noreply.github.com>
2022-11-29 20:27:09 -0600

    Controllers and routes updated

--- nodejs/controllers/consola.js
+++ nodejs/controllers/consola.js
@@ -2,35 +2,70 @@ const path = require("path");
 const { DATEONLY } = require("sequelize");
 const Sequelize = require("sequelize");
 const Diabetes = require("../models/diabetes");
-const {rmSync} = require(fs); //no entiendo porque
+//const {rmSync} = require(fs); //no entiendo porque
 //const Age = require("../utils/database").models.Age;
 
-//CREATE
-exports.postDiabetes =(req,res)=>{
+
+// Proceso cuando se llame a la ruta
+exports.getAltaDiabetes = (req,res)=>{
+    //res.send('<h1>Formulario de consola</h1>')
+    res.sendFile(path.join(__dirname,'..','views','formulario.html'));
+}
+
+
+//Crear registro POST
+exports.postAltaDiabetes =(req,res)=>{
     console.log(req.body)
     Diabetes.create(req,body)
     .then(result=>{
         console.log("Datos creados")
-        res.json(req.body)
+        res.send("Registro exitoso") //Cliente
+        //res.json(req.body)
     })
     .catch((err)=>{
+        console.log(err)
+        //res.json({state:"error"})
+        res.send("Ocurrio un error")//Cliente
+    })
+}
+
+//Get all registros
+exports.getAllDiabetes = (req,res)=>{
+    //SELECT * from Consola;
+    Diabetes.findAll()
+        .then(diabetes=>{
+            console.log("AllDiabetes",diabetes);
+            res.send(diabetes);
+        })
+        .catch(e=>{
+            console.log(e);
+            res.send("Error");
+        })
+}
+
+//FALTA Consultar 1 registro
+exports.getConsultaDiabetes = (req,res)=>{
+    res.send('<h1>Datos de las diabetes</h1>')
+}
+
+//DELETE FROM Consola WHERE id=
+exports.postDeleteDiabetes = (req, res)=>{
+    console.log(req.body)
+    Diabetes.destroy({
+        where:{
+            id: req.body.id
+        }
+    })
+    .then(()=>{
+        console.log("Deleted")
+        res.json({state:"accepted"})
+    })
+    .catch(err =>{
         console.log(err)
         res.json({state:"error"})
     })
 }
 
-//READ
-exports.getDiabetes =(req,res)=>{
-    Diabetes.findAll()
-    .then(vjs=>{
-        console.log(vjs)
-        res.json(vjs)
-    })
-    .catch((err)=>{
-        console.log(err)
-        res.json({state:"error"})
-    })
-}
 
 //UPDATE
 exports.postUpdateDiabetes = (req,res)=>{
@@ -48,24 +83,6 @@ exports.postUpdateDiabetes = (req,res)=>{
         res.json({state:"accepted"})
     })
     .catch((err)=>{
-        console.log(err)
-        res.json({state:"error"})
-    })
-}
-
-//DELETE
-exports.postDeleteDiabetes = (req, res)=>{
-    console.log(req.body)
-    Diabetes.destroy({
-        where:{
-            id: req.body.id
-        }
-    })
-    .then(()=>{
-        console.log("Deleted")
-        res.json({state:"accepted"})
-    })
-    .catch(err =>{
         console.log(err)
         res.json({state:"error"})
     })
--- nodejs/routes/consola.js
+++ nodejs/routes/consola.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const diabetesController= require('../controllers/consola')
+
+//Create,Read,Update,Delete  (CRUD)  Altas Bajas Cambios Consultas
+//Servicio para mostrar el formulario
+router.get('/altaDiabetes',diabetesController.getAltaDiabetes);
+//Servicio para procesar los datos del formulario
+router.post('/altaDiabetes',diabetesController.postAltaDiabetes);
+//Servicio para consultar todos los datos
+router.get('/consultaAllDiabetes',diabetesController.getAllDiabetes);
+//Servicio para consultar un dato
+router.get('/consultaDiabetes',diabetesController.getConsultaDiabetes);
+//Servicio para eliminar un registro por id
+router.post('/deleteDiabetes', diabetesController.postDeleteDiabetes);
+//Servicio para actualizar las consola
+router.post('/updateDiabetes',diabetesController.postUpdateDiabetes);
+
+module.exports = router
